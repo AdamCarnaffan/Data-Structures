a1791b572885e3ee5161fb9a3f9113f998619248
Adam Carnaffan <acarnaffan@hotmail.com>
2019-01-19 07:17:37 -0500

    [Lab2] Recursive Level Order Attempt

--- lab2/lab2_adam/tree.py
+++ lab2/lab2_adam/tree.py
@@ -11,44 +11,36 @@ def sort_lists_by_length(l):
 class tree:
     def __init__(self, x):
         self.store = [x, []]
-        
+
     def AddSuccessor(self, x):
         self.store[1] = self.store[1] + [x]
         return True
-        
-    def Print_DepthFirst(self):
-        disp = ""
-        for val in self.store[1]:
-            disp = disp + "   " + str(val.store[0])
-        print(disp)
+
+    def display(self, indent):
+        print(''.join(['   ' for i in range(0, indent, 1)]) + self.store[0])
+        for t in self.store[1]:
+            t.display(indent + 1)
         return True
-        
-    def Get_LevelOrder(self):
-        tree = []
-        for val in self.store[1]:
-            tree = tree + val.Get_LevelOrder()
-        if len(tree) < 1:
-            return [[self.store[0]]]
-        branches = []
-        # Get longest
-        sorted = sort_lists_by_length(tree)
-        for b in range(0, len(sorted[0]), 1):
-            branches = branches + [[]]
-        for comp in sorted:
-            for x in range(0, len(comp), 1):
-                branches[x] = branches[x] + [comp[x]]
-        print(branches)
-        return [[self.store[0]] + branches]
+
+    def Get_LevelOrder(self, start=True):
+        children = []
+        for t in self.store[1]:
+            children = children + t.Get_LevelOrder(False)
+        return [[self.store[0]] + children]
 
 def main():
-    a = tree(5)
-    b = tree(9)
-    e = tree(4)
-    a.AddSuccessor(tree(20))
+    a = tree("texmf")
+    b = tree("tex")
+    e = tree("plain")
+    e.AddSuccessor(tree("new"))
+    b.AddSuccessor(tree("generic"))
+    b.AddSuccessor(tree("latex"))
     b.AddSuccessor(e)
-    #e.AddSuccessor(tree(12))
+    a.AddSuccessor(tree("doc"))
+    a.AddSuccessor(tree("font"))
+    a.AddSuccessor(tree("source"))
     a.AddSuccessor(b)
-    #a.Print_DepthFirst()
+    a.AddSuccessor(tree("texdoc"))
     print(a.Get_LevelOrder())
-    
+
 main()
