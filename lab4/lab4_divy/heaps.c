4ca4d4d4da536155aacc96b40d416bebb3cb9eb1
Divy <divyr@outlook.com>
2019-02-06 15:58:54 -0500

    .

--- lab4/lab4_divy/heaps.c
+++ lab4/lab4_divy/heaps.c
@@ -14,6 +14,8 @@ int postorder(HeapType *pHeap, int **output, int *o_size);
 int addHeap(HeapType *pHeap, int key);
 int disp_Heap(HeapType *pHeap);
 int disp_array(int *array, int size);
+int exponential(int base, int power);
+int get_depth(int pair);
 
 int initHeap(HeapType *pHeap, int size) {
    if (pHeap == NULL) {
@@ -55,18 +57,25 @@ int inorder(HeapType *pHeap, int **output, int *o_size) {
 }
 
 int addHeap(HeapType *pHeap, int key) {
-   int i = 0, depth = 0;
+   int i = 0, depth = 0, swap = 0, jump = 0;
    if ((pHeap == NULL) || (pHeap->end == pHeap->size)) {
       return -1;
    }
    (pHeap->store)[pHeap->end] = key;
+   i = pHeap->end;
    pHeap->end += 1;
-   i = pHeap->end - 1;
-   depth = i/2;
-   printf("CHILD IS : %d, PARENT IS : %d, DEPTH IS: %d, i is: %d\n", (pHeap->store)[i], (pHeap->store)[i - depth], depth, i);
-   while ((pHeap->store)[i] > (pHeap->store)[i - depth]) {
-      (pHeap->store)[i], (pHeap->store)[i - depth] = (pHeap->store)[i - depth], (pHeap->store)[i];
-      i -= depth;
+   if (i == 1) {
+      return 0;
+   }
+   depth = get_depth((i + 1)/2);
+   jump = exponential(2, depth);
+   while ((pHeap->store)[i] > (pHeap->store)[i - jump]) {
+      swap = (pHeap->store)[i];
+      (pHeap->store)[i] = (pHeap->store)[i - jump];
+      (pHeap->store)[i - jump] = swap;
+      depth -= 1;
+      i -= jump;
+      jump = exponential(2, get_depth((i + 1)/2));
    }
    disp_Heap(pHeap);
    return 0;
@@ -76,7 +85,11 @@ int disp_Heap(HeapType *pHeap) {
    int i = 0;
    printf("[");
    for (i = 0; i < pHeap->end; i++) {
-      printf("%d ", (pHeap->store)[i]);
+      if (i == pHeap->end - 1) {
+         printf("%d", (pHeap->store)[i]);
+         continue;
+      }
+      printf("%d,", (pHeap->store)[i]);
    }
    printf("]\n");
 }
@@ -85,27 +98,41 @@ int disp_array(int *array, int size) {
    int i = 0;
    printf("[");
    for (i = 0; i < size; i++) {
-      printf("%d ", array[i]);
+      if (i == size - 1) {
+         printf("%d", array[i]);
+         continue;
+      }
+      printf("%d,", array[i]);
    }
    printf("]\n");
    return 0;
 }
 
+int exponential(int base, int power) {
+   int i = 0, answer = 1;
+   for (i = 0; i < power; i++) {
+      answer *= base;
+   }
+   return answer;
+}
+
+int get_depth(int pair) {
+   int depth = 0, i = 0;
+   while (exponential(2, depth) <= pair) {
+      depth += 1;
+   }
+   return depth;
+}
+
 int main(void) {
    HeapType *pHeap = (HeapType *)malloc(sizeof(HeapType));
-   int size = 0, o_size = 0, *output;
+   int size = 0, o_size = 0, *output, i = 0;
    printf("Input Size:\n");
    scanf("%d", &size);
    initHeap(pHeap, size);
-   addHeap(pHeap, 10);
-   addHeap(pHeap, 20);
-   addHeap(pHeap, 30);
-   addHeap(pHeap, 12);
-   addHeap(pHeap, 4);
-   addHeap(pHeap, 7);
-   addHeap(pHeap, 18);
-   addHeap(pHeap, 19);
-   addHeap(pHeap, 100);
+   for (i = 0; i < 16; i++) {
+      addHeap(pHeap, i);
+   }
    //inorder(pHeap, &output, &o_size);
    //disp_Heap(pHeap);
    //disp_array(output, size);
