12a96bc40d60b624dae71a8ee14d24d54f6c2caf
Divy <divyr@outlook.com>
2019-02-08 11:14:13 -0500

    .

--- lab4/lab4_divy/heaps.c
+++ lab4/lab4_divy/heaps.c
@@ -12,6 +12,7 @@ int inorder(HeapType *pHeap, int **output, int *o_size);
 int preorder(HeapType *pHeap, int **output, int *o_size);
 int postorder(HeapType *pHeap, int **output, int *o_size);
 int addHeap(HeapType *pHeap, int key);
+int findHeap(HeapType *pHeap, int key);
 int disp_Heap(HeapType *pHeap);
 int disp_array(int *array, int size);
 
@@ -28,8 +29,8 @@ int initHeap(HeapType *pHeap, int size) {
    return 0;
 }
 
-int inorder(HeapType *pHeap, int **output, int *o_size) {
-   int index = 0, node = 0, counter = 0, side = 0, depth = 1; /* left = 1, right = 2 */
+int inorder(HeapType *pHeap, int **output, int *o_size) {  /* NLR */
+   int index = 0, node = 0, counter = 0, side = 1, depth = 1; /* left = 1, right = 2 */
    if (pHeap == NULL) {
       return -1;
    }
@@ -38,16 +39,116 @@ int inorder(HeapType *pHeap, int **output, int *o_size) {
    if (*output == NULL) {
       return -1;
    }
-   for (counter = 0; counter < *o_size; counter++) {
+   while (counter < *o_size) {
       if (node == 1) {
+         while (index % 2 == 0) {
+            index = (index - side)/2;
+         }
          index = (index - 1)/2;
-         (*output)[counter] = (*output)[counter] = (pHeap->store)[index];
-         side = 1;
-         printf("Output: %d", counter), disp_array(*output, counter);
+         (*output)[counter] = (pHeap->store)[index];
+         node = 0;
+         side = 2;
+         counter += 1;
          continue;
       }
+      while (2*index + side < *o_size) {
+         index = 2*index + side;
+         if (side % 2 == 0) {
+            if (2*index + 1 >= *o_size) { node = 1; }
+            else { 
+               side = 1;
+               continue;
+            }
+            break;
+         }
+      }
       (*output)[counter] = (pHeap->store)[index];
-      printf("Output: %d", counter), disp_array(*output, counter);
+      counter += 1;
+      if (side == 1) { node = 1; }
+   }
+   return 0;
+}
+
+int preorder(HeapType *pHeap, int **output, int *o_size) {  /*NLR*/
+   int index = 0, side = 1, counter = 0, node = 1;
+   if (pHeap == NULL) {
+      return -1;
+   }
+   *o_size = pHeap->end;
+   *output = (int *)malloc(sizeof(int) * (*o_size));
+   if (*output == NULL) {
+      return -1;
+   }
+   while (counter < *o_size) {
+      if (node == 1) {
+         if (side == 1) {
+            (*output)[counter] = (pHeap->store)[index];
+            counter += 1;
+         }
+         else {
+            while (index % 2 == 0) {
+               index = (index - side)/2;
+            }
+            index = (index - 1)/2;
+         }
+         node = 0;
+         continue;
+      }
+      while (2*index + side < *o_size) {
+         index = 2*index + side;
+         (*output)[counter] = (pHeap->store)[index];
+         counter += 1;
+         if (side % 2 == 0) {
+            if (2*index + 1 >= *o_size) {
+               break;
+            }
+            else {
+               side = 1;
+            }
+         }
+      }
+      node = 1;
+      if (side == 1) { side = 2; }
+   }
+   return 0;
+}
+
+int postorder(HeapType *pHeap, int **output, int *o_size) {  /* LRN */
+   int index = 0, side = 1, counter = 0, node = 0;
+   if (pHeap == NULL) {
+      return -1;
+   }
+   *o_size = pHeap->end;
+   *output = (int *)malloc(sizeof(int) * (*o_size));
+   if (*output == NULL) {
+      return -1;
+   } 
+   while (counter < *o_size) {
+      if (node == 1) {
+         while (index % 2 == 0) {
+            index = (index - side)/2;
+            (*output)[counter] = (pHeap->store)[index];
+            counter += 1;
+         }
+         index = (index - 1)/2;
+         side = 2;
+         node = 0;
+         continue;
+      }
+      while (2*index + side < *o_size) {
+         index = 2*index + side;
+         if (side % 2 == 0) {
+            if (2*index + 1 >= *o_size) {
+               break;
+            }
+            else {
+               side = 1;
+            }
+         }
+      }
+      (*output)[counter] = (pHeap->store)[index];
+      counter += 1;
+      node = 1;
    }
    return 0;
 }
@@ -72,6 +173,22 @@ int addHeap(HeapType *pHeap, int key) {
       parent = (index + 1)/2 - 1;
    }
    return 0;
+}
+
+int findHeap(HeapType *pHeap, int key) {
+   int index = 0, c1 = 0, c2 = 0, side = 1, node = 0;
+   if (pHeap == NULL) {
+      return -1;
+   }
+   while (1) {
+      if (node == 1) {
+         continue;
+      }
+      while ((2*index + side < pHeap->end) && ( key > (pHeap->store)[2*index + side])) {
+         index = 2*index + side;
+      }
+      if (key == )
+   }
 }
 
 int disp_Heap(HeapType *pHeap) {
@@ -111,7 +228,7 @@ int main(void) {
    for (i = 0; i < size; i++) {
       addHeap(pHeap, i);
    }
-   inorder(pHeap, &output, &o_size);
+   preorder(pHeap, &output, &o_size);
    printf("Heap: "), disp_Heap(pHeap);
    printf("Output: "), disp_array(output, size);
    return 0;
