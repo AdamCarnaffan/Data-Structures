5eed5d8324c5a9e9a7f706a6504ebd2b66e567b1
Adam Carnaffan <acarnaffan@hotmail.com>
2019-03-21 02:02:31 -0400

    [lab7] Complete save Quicksort

--- lab7/lab7_adam/sortlib.py
+++ lab7/lab7_adam/sortlib.py
@@ -7,13 +7,14 @@ def selection_sort(u):
       swp = u[x]
       u[x] = u[selected]
       u[selected] = swp
-      print(u)
    return True
 
 def heap_sort(u): # Use max heap to make ASC order
    heapify(u)
    for ind in range(len(u)-1, -1, -1):
-      
+      swp = u[0]
+      u[0] = u[ind]
+      u[ind] = swp
       reheapify(u, ind-1)
    pass
 
@@ -29,9 +30,47 @@ def heapify(u):
    return True
 
 def reheapify(u, end):
-   pass
+   keep = u[end+1:len(u)]
+   heap = u[0:end+1]
+   heapify(heap)
+   for v in range(0, len(heap), 1):
+      u[v] = heap[v]
+   for i in range(0, len(keep), 1):
+      u[len(heap) + i] = keep[i]
+   return True
 
 def merge_sort(u):
+   part = 1
+   while part < len(u):
+      for select in range(0, int(len(u)/part), 2):
+         x = select*part
+         y = (select+1)*part
+         if y >= len(u):
+            break
+         wk = []
+         while x < (select+1)*part or (y < (select+2)*part and y < len(u)):
+            if y >= (select+2)*part or y >= len(u): 
+               # Add x
+               wk = wk + [u[x]]
+               x = x + 1
+            elif x >= (select+1)*part:
+               # Add y
+               wk = wk + [u[y]]
+               y = y + 1
+            else:
+               if u[x] < u[y]:
+                  # Add x
+                  wk = wk + [u[x]]
+                  x = x + 1
+               else:
+                  # Add y
+                  wk = wk + [u[y]]
+                  y = y + 1
+         for ind in range(select*part, (select+2)*part, 1):
+            if ind >= len(u):
+               break
+            u[ind] = wk[ind - select*part]
+      part = part * 2
    return True
 
 def quick_sort(u, ini, fin):
@@ -55,9 +94,6 @@ def helper_shift(u, pos):
       return True
    return False
 
-def helper_unshift(u, pos):
-   pass
-
 def helper_get_parent(ind):
    if ind == 0:
       return -1
@@ -65,9 +101,33 @@ def helper_get_parent(ind):
       return int((ind-1)/2)
 
 def main():
-   v = [1, 6, 8, 2]
-   heapify(v)
-   print(v)
-   return
+   v1 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+   v2 = [100, 1, 1000, 9, 8, 7, 2, 2000, 10]
+   v3 = [100, 10, 1000, 9, 8, 7, 2, 6, 5, 2, 3, 1]
+
+   print("SELECTION_SORT:")
+   for i in [v1, v2, v3]:
+      x = list(i)
+      selection_sort(x)
+      print(x)
+   
+   print("\nMERGE_SORT:")
+   for i in [v1, v2, v3]:
+      x = list(i)
+      merge_sort(x)
+      print(x)
+
+   print("\nHEAP_SORT:")
+   for i in [v1, v2, v3]:
+      x = list(i)
+      heap_sort(x)
+      print(x)
+   
+   # print("\nQUICK_SORT:")
+   # for i in [v1, v2, v3]:
+   #    x = list(i)
+   #    quick_sort(x, 0, len(x) - 1)
+   #    print(x)
+   return True
 
 main()
